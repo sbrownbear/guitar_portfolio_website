b084de7ccc7ac7f0686bd674706c60a2dc6356d3
Sergey Konoplev <sergey.gsarh@gmail.com>
2022-12-21 01:11:31 +0300

    added smooth scrolling

--- js/script.js
+++ js/script.js
@@ -15,3 +15,14 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+$(document).ready(function(){
+    $("a[href*='#']").on("click", function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 1000);
+      e.preventDefault();
+      return false;
+    });
+  });
